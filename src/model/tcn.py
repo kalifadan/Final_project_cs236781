63ccbe645383a107efd06631c92bc8bb52e39d17
Miki Mints <mikimn1999@gmail.com>
2020-09-10 15:41:33 +0300

    Fix init()s in tcn

--- src/model/tcn.py
+++ src/model/tcn.py
@@ -5,7 +5,7 @@ from torch.nn.utils import weight_norm
 
 class Chomp1d(nn.Module):
     def __init__(self, chomp_size):
-        super(Chomp1d, self).__init__()
+        super().__init__()
         self.chomp_size = chomp_size
 
     def forward(self, x):
@@ -14,7 +14,7 @@ class Chomp1d(nn.Module):
 
 class TemporalBlock(nn.Module):
     def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2):
-        super(TemporalBlock, self).__init__()
+        super().__init__()
         self.conv1 = weight_norm(nn.Conv1d(n_inputs, n_outputs, kernel_size,
                                            stride=stride, padding=padding, dilation=dilation))
         self.chomp1 = Chomp1d(padding)
@@ -27,8 +27,8 @@ class TemporalBlock(nn.Module):
         self.relu2 = nn.ReLU()
         self.dropout2 = nn.Dropout(dropout)
 
-        self.net = nn.Sequential(self.conv1, self.chomp1, self.relu1, self.dropout1,
-                                 self.conv2, self.chomp2, self.relu2, self.dropout2)
+        self.net = nn.Sequential(self.conv1, self.chomp1, self.relu1, self.dropout1, nn.BatchNorm1d(n_outputs),
+                                 self.conv2, self.chomp2, self.relu2, self.dropout2, nn.BatchNorm1d(n_outputs))
         self.downsample = nn.Conv1d(n_inputs, n_outputs, 1) if n_inputs != n_outputs else None
         self.relu = nn.ReLU()
         self.init_weights()
@@ -47,7 +47,7 @@ class TemporalBlock(nn.Module):
 
 class TemporalConvNet(nn.Module):
     def __init__(self, num_inputs, num_channels, kernel_size=2, dropout=0.2):
-        super(TemporalConvNet, self).__init__()
+        super().__init__()
         layers = []
         num_levels = len(num_channels)
         for i in range(num_levels):
